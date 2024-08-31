2185fef04bceb877539a3352147f3956046d559e
N1CKZ3V <nico_03_sebas@hotmail.com>
2024-08-31 16:26:57 -0500

    Cambios antes de comenzar parte III

--- Blacklist/src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
+++ Blacklist/src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.blacklistvalidator;
 
 import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
@@ -13,52 +8,34 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author hcadavid
- */
 public class HostBlackListsValidator {
-    private int checkedCount=0;
-    private int ocurrencesCount=0;
-    private static final int BLACK_LIST_ALARM_COUNT=5;
-    
-    /**
-     * Check the given host's IP address in all the available black lists,
-     * and report it as NOT Trustworthy when such IP was reported in at least
-     * BLACK_LIST_ALARM_COUNT lists, or as Trustworthy in any other case.
-     * The search is not exhaustive: When the number of occurrences is equal to
-     * BLACK_LIST_ALARM_COUNT, the search is finished, the host reported as
-     * NOT Trustworthy, and the list of the five blacklists returned.
-     * @param ipaddress suspicious host's IP address.
-     * @return  Blacklists numbers where the given host's IP address was found.
-     */
+    private int checkedCount = 0;
+    private int ocurrencesCount = 0;
+    private static final int BLACK_LIST_ALARM_COUNT = 5;
+    private final Object lock = new Object();
 
-
-    public List<Integer> checkHost(String ipaddress, int N){
-        
-        LinkedList<Integer> blackListOcurrences=new LinkedList<>();
-        
-        HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
-
+    public List<Integer> checkHost(String ipaddress, int N) {
+        LinkedList<Integer> blackListOcurrences = new LinkedList<>();
+        HostBlacklistsDataSourceFacade skds = HostBlacklistsDataSourceFacade.getInstance();
         int partir = skds.getRegisteredServersCount() / N;
-
         List<MaliciousHostCounter> maliciousHosts = new ArrayList<>();
-        
-        int start,end;
+        int start, end;
 
-        for (int i=0;i < N ;i++){
-           start = i * partir;
-           if (i == N - 1){
+        for (int i = 0; i < N; i++) {
+            start = i * partir;
+            if (i == N - 1) {
                 end = skds.getRegisteredServersCount();
-           }else{
+            } else {
                 end = (i + 1) * partir;
-           }
-           MaliciousHostCounter host = new MaliciousHostCounter(skds, ipaddress, start, end);
-           maliciousHosts.add(host);
+            }
+            MaliciousHostCounter host = new MaliciousHostCounter(skds, ipaddress, start, end, this);
+            maliciousHosts.add(host);
         }
+
         for (MaliciousHostCounter host : maliciousHosts) {
             host.start();
         }
+
         for (MaliciousHostCounter host : maliciousHosts) {
             try {
                 host.join();
@@ -67,29 +44,34 @@ public class HostBlackListsValidator {
             }
 
             blackListOcurrences.addAll(host.getOccurrences());
-            
-                for (Integer e: host.getOccurrences()) {
-                    if (!blackListOcurrences.contains(e)) {
-                        blackListOcurrences.add(e);
-                    }
-                }
-                checkedCount += host.getCheckedCount();
-                ocurrencesCount += host.getFoundCount();
+            checkedCount += host.getCheckedCount();
+            ocurrencesCount += host.getFoundCount();
         }
-        if (ocurrencesCount>=BLACK_LIST_ALARM_COUNT){
+
+        if (ocurrencesCount >= BLACK_LIST_ALARM_COUNT) {
             skds.reportAsNotTrustworthy(ipaddress);
-        }
-        else{
+        } else {
             skds.reportAsTrustworthy(ipaddress);
-        }                
+        }
+
         LOG.log(Level.INFO, "Checked Black Lists:{0} of {1}", new Object[]{checkedCount, skds.getRegisteredServersCount()});
-        
         return blackListOcurrences;
     }
-    
-    
+
+    public synchronized void reportOccurrence(int serverIndex) {
+        synchronized (lock) {
+            ocurrencesCount++;
+            if (ocurrencesCount >= BLACK_LIST_ALARM_COUNT) {
+                lock.notifyAll();
+            }
+        }
+    }
+
+    public synchronized boolean shouldStop() {
+        synchronized (lock) {
+            return ocurrencesCount >= BLACK_LIST_ALARM_COUNT;
+        }
+    }
+
     private static final Logger LOG = Logger.getLogger(HostBlackListsValidator.class.getName());
-    
-    
-    
 }
--- Blacklist/src/main/java/edu/eci/arsw/blacklistvalidator/MaliciousHostCounter.java
+++ Blacklist/src/main/java/edu/eci/arsw/blacklistvalidator/MaliciousHostCounter.java
@@ -1,74 +1,52 @@
 package edu.eci.arsw.blacklistvalidator;
+
 import java.util.LinkedList;
 import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
 
-/**
- * Clase de tipo Thread que representa el ciclo de vida de un hilo.
- * Realiza la búsqueda de un segmento del conjunto de servidores disponibles.
- */
 public class MaliciousHostCounter extends Thread {
-    private String ip;
-    private int startIdx, endIdx;
+    private final String ip;
+    private final int startIdx, endIdx;
     private int foundCount;
     private int checkedCount;
-    private HostBlacklistsDataSourceFacade dataSource;
-    private LinkedList<Integer> occurrences;
-    /**
-     * Constructor de la clase.
-     * @param dataSource Fuente de datos de listas negras.
-     * @param ip Dirección IP a buscar.
-     * @param startIdx Índice de inicio del segmento a revisar.
-     * @param endIdx Índice de fin del segmento a revisar.
-     */
-    public MaliciousHostCounter(HostBlacklistsDataSourceFacade dataSource, String ip, int startIdx, int endIdx) {
+    private final HostBlacklistsDataSourceFacade dataSource;
+    private final LinkedList<Integer> occurrences;
+    private final HostBlackListsValidator validator;
+
+    public MaliciousHostCounter(HostBlacklistsDataSourceFacade dataSource, String ip, int startIdx, int endIdx, HostBlackListsValidator validator) {
         this.dataSource = dataSource;
         this.ip = ip;
         this.startIdx = startIdx;
-        this.endIdx = endIdx;   
+        this.endIdx = endIdx;
+        this.validator = validator;
         this.occurrences = new LinkedList<>();
         foundCount = 0;
         checkedCount = 0;
     }
 
-    /**
-     * Ejecución del Thread.
-     */
     @Override
     public void run() {
-        int localFoundCount = 0;
-        int localCheckedCount = 0;
-    
-        for (int i = startIdx; i < endIdx; i++) {
-            localCheckedCount++;
+        for (int i = startIdx; i < endIdx && !validator.shouldStop(); i++) {
+            checkedCount++;
             if (dataSource.isInBlackListServer(i, ip)) {
                 occurrences.add(i);
-                localFoundCount++;
-            } 
+                foundCount++;
+                validator.reportOccurrence(i);
+
+                if (validator.shouldStop()) {
+                    break;
+                }
+            }
         }
-        this.checkedCount = localCheckedCount;
-        this.foundCount = localFoundCount;
     }
-    
-    /**
-     * Método que permite preguntar cuántas ocurrencias de servidores maliciosos ha encontrado.
-     * @return cantidad de ocurrencias encontradas.
-     */
+
     public int getFoundCount() {
         return foundCount;
     }
 
-    /**
-     * Método que permite preguntar cuántas listas de servidores maliciosos ha revisado.
-     * @return cantidad de listas revisadas.
-     */
     public int getCheckedCount() {
         return checkedCount;
     }
 
-    /**
-     * Método que permite obtener las posiciones dentro de la lista donde se encontró la IP.
-     * @return posiciones dentro de la lista (int).
-     */
     public LinkedList<Integer> getOccurrences() {
         return occurrences;
     }
